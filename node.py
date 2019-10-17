4540a783869d3c73fe1788dfab23064be5e97ff7
Stanley Tang <stang0926@gmail.com>
2019-10-17 15:36:30 -0400

    edit

--- node.py
+++ node.py
@@ -1,19 +1,11 @@
 class Node:
-<<<<<<< HEAD
 
     def __init__(self, value):
         self.value = value
         self.nextNode = None
 
-    def 
 
 node1 = Node(50)
 node2 = Node(30)
 node1.nextNode = node2
 print(node1.value)
-=======
-    
-    def __init__(self, prevNode, nextNode):
-        self.prevNode = prevNode
-        self.nextNode = nodeNode
->>>>>>> master
