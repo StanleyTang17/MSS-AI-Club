395dc7f742ceb21be8d41202ade4fe345c7a6b3b
Stanley Tang <stang0926@gmail.com>
2019-10-17 15:26:51 -0400

    edit

---
+++ node.py
@@ -0,0 +1,12 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+
+    def 
+
+node1 = Node(50)
+node2 = Node(30)
+node1.nextNode = node2
+print(node1.value)
